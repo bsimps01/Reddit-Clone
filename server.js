44e7c4b554f0927fa845fb8edf1a134f08fcced6
bsimps01 <b.albertsimpson@gmail.com>
2021-04-16 23:31:54 -0700

    added post objects

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express')
-const app = express()
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const app = express()
 const port = 3000
-// Middleware
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+//Database
+require('./controllers/posts.js')(app);
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+})
+
+//Server Start
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
