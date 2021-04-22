2fa33137790412c0ac902ed79c70084216b29b2a
bsimps01 <b.albertsimpson@gmail.com>
2021-04-21 17:05:36 -0700

    fixed some parts

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,25 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
-  app.get('/', (req, res) => {
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+    console.log(post)
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      console.log(err)
+      console.log(post)
+      return res.redirect(`/`);
+    })
+  });
+
+  app.get('/posts/find', (req, res) => {
     Post.find({}).lean()
       .then(posts => {
-        res.render('post-index', { posts });
+        res.render('posts-index', { posts });
       })
       .catch(err => {
         console.log(err.message);
@@ -14,25 +29,11 @@ module.exports = (app) => {
 
   app.get("/posts/:id", function(req, res) {
     // LOOK UP THE POST
-    Post.findById(req.params.id).lean()
-      .then(post => {
-        res.render("posts-show", { post });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  });
-
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
-
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
+    Post.findById(req.params.id).lean().populate('comments').then((post) => {
+      res.render('posts-show', { post })
+    }).catch((err) => {
+      console.log(err.message)
     })
+    
   });
-
 };
--- server.js
+++ server.js
@@ -1,37 +1,44 @@
-const express = require('express')
-const exphbs  = require('express-handlebars');
+// Require Libraries
+const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-const app = express()
-const port = 3000
+
+// App Setup
+const app = express();
 
 // Use Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Add after body parser initialization!
 app.use(expressValidator());
 
 // Set db
 require('./data/reddit-db');
-//Database
-require('./controllers/posts.js')(app);
 
+// Middleware
+const exphbs  = require('express-handlebars');
 
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static('public'));
 
-// Routes
+
 app.get('/', (req, res) => {
   res.render('home')
 })
 
 app.get('/posts/new', (req, res) => {
-    res.render('posts-new');
-})
+    res.render('posts-new')
+}); 
 
-//Server Start
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Controllers
+require('./controllers/posts.js')(app);
+//require('./controllers/comments.js')(app);
+
+// Start Server
+app.listen(3000, () => {
+  console.log('Reddit Clone on port localhost:3000!');
+});
+
+module.exports = app
